008d76dfbbb90acb2ab420d96a6699804880bc77
koulan789 <393240588@qq.com>
2016-12-04 14:45:55 +0800

    Update UsersideModel.class.php

--- Application/Admin/Model/UsersideModel.class.php
+++ Application/Admin/Model/UsersideModel.class.php
@@ -1,14 +1,6 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Administrator
- * Date: 16-1-26
- * Time: 上午9:59
- * To change this template use File | Settings | File Templates.
- */
 
 namespace Admin\Model;
-
 
 use Think\Model;
 
