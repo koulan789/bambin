4cde867583faf5c91707b2da8dff4309b70d586b
koulan789 <393240588@qq.com>
2016-12-04 14:40:41 +0800

    Update UserController.class.php

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Administrator
- * Date: 16-2-29
- * Time: 下午12:52
- * To change this template use File | Settings | File Templates.
- */
 
 namespace Admin\Controller;
 
