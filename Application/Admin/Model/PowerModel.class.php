5153e39e0696d0b9c23b387a3640ba3e76214547
koulan789 <393240588@qq.com>
2016-12-04 14:44:14 +0800

    Update PowerModel.class.php

--- Application/Admin/Model/PowerModel.class.php
+++ Application/Admin/Model/PowerModel.class.php
@@ -1,14 +1,6 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Administrator
- * Date: 16-1-28
- * Time: 下午3:08
- * To change this template use File | Settings | File Templates.
- */
 
 namespace Admin\Model;
-
 
 use Think\Model;
 
