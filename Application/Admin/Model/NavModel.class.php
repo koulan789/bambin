b8e5789fdf070f175aad9e867c39c22f0203a078
koulan789 <393240588@qq.com>
2016-12-04 14:43:30 +0800

    Update NavModel.class.php

--- Application/Admin/Model/NavModel.class.php
+++ Application/Admin/Model/NavModel.class.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Administrator
- * Date: 16-1-22
- * Time: 下午2:04
- * To change this template use File | Settings | File Templates.
- */
+
 namespace Admin\Model;
 use Think\Model;
 class NavModel extends Model{
