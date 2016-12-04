13c4214bd1d657ac84e2150caeda986790b25060
koulan789 <393240588@qq.com>
2016-12-04 14:39:33 +0800

    Update NewsController.class.php

--- Application/Admin/Controller/NewsController.class.php
+++ Application/Admin/Controller/NewsController.class.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Administrator
- * Date: 16-1-29
- * Time: 下午3:31
- * To change this template use File | Settings | File Templates.
- */
+
 
 namespace Admin\Controller;
 
