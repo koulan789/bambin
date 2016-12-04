a086f21bf72afde37e85bec9747da81fcb59f3c9
koulan789 <393240588@qq.com>
2016-12-04 14:43:59 +0800

    Update NewsViewModel.class.php

--- Application/Admin/Model/NewsViewModel.class.php
+++ Application/Admin/Model/NewsViewModel.class.php
@@ -1,14 +1,6 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Administrator
- * Date: 16-1-29
- * Time: 下午4:38
- * To change this template use File | Settings | File Templates.
- */
 
 namespace Admin\Model;
-
 
 use Think\Model\ViewModel;
 
