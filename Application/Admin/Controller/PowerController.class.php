95fd5bdf1668b5782bfee5e8772ce6abb2a34210
koulan789 <393240588@qq.com>
2016-12-04 14:40:17 +0800

    Update PowerController.class.php

--- Application/Admin/Controller/PowerController.class.php
+++ Application/Admin/Controller/PowerController.class.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Administrator
- * Date: 16-1-28
- * Time: 下午1:49
- * To change this template use File | Settings | File Templates.
- */
 
 namespace Admin\Controller;
 
