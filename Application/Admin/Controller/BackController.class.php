cea3db7e247153cff523a6370fdc6b4008295880
koulan789 <393240588@qq.com>
2016-12-04 14:37:21 +0800

    Update BackController.class.php

--- Application/Admin/Controller/BackController.class.php
+++ Application/Admin/Controller/BackController.class.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Administrator
- * Date: 16-1-22
- * Time: 下午12:39
- * To change this template use File | Settings | File Templates.
- */
+
 
 namespace Admin\Controller;
 
